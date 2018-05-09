9f8eb7c1414d8f399c123d8f10ca99606dd0d691
Hyuan <h_y_12@163.com>
2018-05-09 19:43:22 +0800

    添加字體大小

--- src/main/java/com/iss/core/index/IndexController.java
+++ src/main/java/com/iss/core/index/IndexController.java
@@ -1,13 +1,16 @@
 package com.iss.core.index;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping(value = "/index")
-    public String index() {
+    public String index(Model model) {
+        System.out.println("mini pro 首页方法...");
+        model.addAttribute("info", "mini pro 首页方法...");
         return "index";
     }
 }
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -1,9 +1,10 @@
 <#include "./core/baseTemplate.ftl"/>
 <@htmlHead title="minipro">
-<script type="text/javascript">
-    console.log('minipro 模板日志');
-</script>
+    <script type="text/javascript">
+        console.log('minipro 模板日志');
+    </script>
 </@htmlHead>
 <@htmlBody>
     this minipro index page...
+    ${info}
 </@htmlBody>
